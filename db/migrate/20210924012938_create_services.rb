b7024a1da6e2bb2102db7556b79a105eba04b5af
Collin Gardner <collingardner5@gmail.com>
2021-09-23 21:19:00 -0600

    initial setup

---
+++ db/migrate/20210924012938_create_services.rb
@@ -0,0 +1,12 @@
+class CreateServices < ActiveRecord::Migration[6.1]
+  def change
+    create_table :services do |t|
+      t.string :job_type
+      t.integer :cost
+      t.string :date
+      t.belongs_to :worker, null: false, foreign_key: true
+     
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,34 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do
+  @worker = Worker.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name ,
+    phone_number: Faker::PhoneNumber.cell_phone,
+  )
+
+  5.times do
+    Service.create(
+      job_type: Faker::Job.field,
+      cost: Faker::Business.credit_card_type,
+      date: Faker::Date.between,
+      worker_id: @worker.id
+    )
+
+    5.times do
+      Comment.create(
+        topic: Faker::Job.field,
+        description: Faker::Company.bs,
+        address: Faker::Address.,
+        service_id: @service.id
+  end 
+end 
+
+puts "Data Seeded"
